11e56841f1e93e1f21b5da01d9c7343c95ca14fa
Felix Fontein <felix@fontein.de>
2021-01-31 19:09:56 +0100

    Linting.

--- tests/unit/plugins/modules/helper.py
+++ tests/unit/plugins/modules/helper.py
@@ -1,6 +1,12 @@
+# (c) 2021 Felix Fontein <felix@fontein.de>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 try:
     import lxml.etree
-except:
+except ImportError:
     # should be handled in module importing this one
     pass
 
